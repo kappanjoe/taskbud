3bde953bc2389a1907b7d052792db066c5603918
Joe Van Alstyne <6261485+kappanjoe@users.noreply.github.com>
2023-04-17 18:55:57 +0900

    fix(client): dynamically spawn socket based on env

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,4 +1,8 @@
-import { io, Socket } from 'socket.io-client';
+import { io, Socket, ManagerOptions, SocketOptions } from 'socket.io-client';
 import { ServerToClientEvents, ClientToServerEvents } from './types/socket';
 
-export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io('localhost:4000'); // UPDATE BEFORE RELEASE
+const URL = (process.env.NODE_ENV === 'production')
+  ? undefined
+  : 'http://localhost:4000' as Partial<ManagerOptions & SocketOptions>;
+
+export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(URL);
