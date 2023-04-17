6bfa67885493f3157d9354fc64e5026f45c67bd1
Joe Van Alstyne <6261485+kappanjoe@users.noreply.github.com>
2023-04-17 21:51:53 +0900

    fix(client): connect socket to api url

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -2,7 +2,7 @@ import { io, Socket, ManagerOptions, SocketOptions } from 'socket.io-client';
 import { ServerToClientEvents, ClientToServerEvents } from './types/socket';
 
 const URL = process.env.NODE_ENV === 'production'
-  ? undefined
+  ? process.env.REACT_APP_API_URL as Partial<ManagerOptions & SocketOptions>
   : 'http://localhost:4000' as Partial<ManagerOptions & SocketOptions>;
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(URL);
