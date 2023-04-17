5d7b5639575c646009ab120846e878a43daecda6
Joe Van Alstyne <6261485+kappanjoe@users.noreply.github.com>
2023-04-17 21:30:14 +0900

    build(client): serve client using express

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,7 +1,7 @@
 import { io, Socket, ManagerOptions, SocketOptions } from 'socket.io-client';
 import { ServerToClientEvents, ClientToServerEvents } from './types/socket';
 
-const URL = (process.env.NODE_ENV === 'production')
+const URL = process.env.NODE_ENV === 'production'
   ? undefined
   : 'http://localhost:4000' as Partial<ManagerOptions & SocketOptions>;
 
