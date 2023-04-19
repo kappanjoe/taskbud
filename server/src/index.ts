33a4d91d08f1a1018d1e9fabdfaf9fc9a7cce75f
Joe Van Alstyne <6261485+kappanjoe@users.noreply.github.com>
2023-04-19 22:53:10 +0900

    fix(server): successfully add task to mongodb

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,7 @@
 import { Server } from 'socket.io';
 import { client } from './mongodb';
+import { error } from 'console';
+import { Document } from 'mongodb';
 require('dotenv').config();
 
 const PORT = parseInt(process.env.PORT!) || 4000;
@@ -65,25 +67,86 @@ io.on('connection', (socket) => {
 		}
 	});
 
-	socket.on('upsertTask', async (userId: string, task: { _id: string }, cb: (taskList: any) => void) => {
+	socket.on('addTask', async (userId: string, task: Document, cb: (taskList: any) => void) => {
 		try {
 			await client.connect();
-			const response = await client
+			const oldList = await client
 				.db(process.env.MONGO_DB_NAME)
 				.collection('task-lists')	
-				.findOneAndUpdate(
-						{ owner_id: userId },
-						{ $set: { "tasks.$[task]": task } },
-						{ arrayFilters: [ { task: { _id: task._id } } ], upsert: true, returnDocument: "after" }
+				.findOne(
+						{ owner_id: userId }
 					);
-			
-			console.log("Task upserted: ", response);
-			if (response?.value) { cb(response.value) };
+
+			let newList: Document;
+
+			if (oldList) {
+				newList = {
+					_id: oldList._id,
+					owner_id: oldList.owner_id,
+					tasks: oldList.tasks.concat(task)
+				};
+
+				const response = await client
+					.db(process.env.MONGO_DB_NAME)
+					.collection('task-lists')
+					.findOneAndReplace(
+						{ _id: oldList._id },
+						newList,
+						{ returnDocument: "after" }
+					);
+				console.log("Task added: ", response.value);
+				cb(response.value);
+			} else {
+				throw "Target list not found.";
+			}
 		} catch (err) {
 			console.dir(err);
 		} finally {
 			await client.close();
 		}
-	})
+	});
+
+	socket.on('updateTask', async (userId: string, task: Document, cb: (taskList: any) => void) => {
+		try {
+			await client.connect();
+			const oldList = await client
+				.db(process.env.MONGO_DB_NAME)
+				.collection('task-lists')	
+				.findOne(
+						{ owner_id: userId }
+					);
+
+			let newList: Document;
+
+			if (oldList) {
+				newList = {
+					_id: oldList._id,
+					owner_id: oldList.owner_id,
+					tasks: oldList.tasks.map( (oldTask: Document) => {
+							return oldTask._id === task._id
+								? task
+								: oldTask;
+						})
+				};
+
+				const response = await client
+					.db(process.env.MONGO_DB_NAME)
+					.collection('task-lists')
+					.findOneAndReplace(
+						{ _id: oldList._id },
+						newList,
+						{ returnDocument: "after" }
+					);
+				console.log("Task updated: ", response.value);
+				cb(response.value);
+			} else {
+				throw "Target list not found.";
+			}
+		} catch (err) {
+			console.dir(err);
+		} finally {
+			await client.close();
+		}
+	});
 
 });
