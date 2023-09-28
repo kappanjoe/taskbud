2ce1b81a64fcdd0515a5dd0b636be75e64f51b00
Joe Van Alstyne <6261485+kappanjoe@users.noreply.github.com>
2023-09-28 15:57:18 +0900

    chore: style/layout for Welcome page

--- client/src/pages/Welcome.tsx
+++ client/src/pages/Welcome.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import './Welcome.css';
+import screenshot from '../images/taskbud-screenshot.png';
 
 function Welcome() {
   return (
-    <div className="home-container">
-      <div className="home-navbar">
-        <h1 className="home-header">task bud</h1>
+    <>
+      <div className="welcome-wrapper">
+        <div className="home-navbar">
+          <h1 className="home-header">task bud</h1>
+        </div>
+        <div>
+          <p>
+            <i>task bud</i> is a<br/>
+            task management tool<br/>
+            for keeping yourself and<br/>
+            your friends accountable.
+          </p>
+          <br/>
+          <button className="button-primary" type="submit">Sign Up</button>
+          <button className="button-secondary" type="submit">Log In</button>
+        </div>
+        <img className="welcome-screenshot" src={screenshot} alt=""/>
       </div>
-      <div>
-        task bud is a social task management tool that helps you keep yourself and your friends accountable.
-      </div>
-      <button className="button-primary" type="submit">Sign Up</button>
-      <button className="button-secondary" type="submit">Log In</button>
-    </div>
+    </>
   )
 };
 
