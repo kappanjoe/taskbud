53a3fd748057a1c3a06b32b758566954cb14658e
Joe Van Alstyne <6261485+kappanjoe@users.noreply.github.com>
2023-09-28 15:15:18 +0900

    chore: layout Welcome page

--- client/src/pages/Welcome.tsx
+++ client/src/pages/Welcome.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 function Welcome() {
   return (
-    <div>Welcome</div>
+    <div className="home-container">
+      <div className="home-navbar">
+        <h1 className="home-header">task bud</h1>
+      </div>
+      <div>
+        task bud is a social task management tool that helps you keep yourself and your friends accountable.
+      </div>
+      <button className="button-primary" type="submit">Sign Up</button>
+      <button className="button-secondary" type="submit">Log In</button>
+    </div>
   )
 };
 
